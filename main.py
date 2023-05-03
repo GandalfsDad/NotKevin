41e3ee7be5098de5b2e448f5fb262882a398561a
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-05-03 18:52:48 +1000

    Add runner script

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
 from NotKevin.Agent import Agent
+import argparse
 
-def main():
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--clear", dest = "clear",help="Clears the memory before running", action="store_true")
+
+def main(clear = False):
     agent = Agent(autosave=True)
+    
+    if clear:
+        agent.clear_memory(save=True)
     agent.run()
 
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+    clear = False
+    if args.clear:
+        clear = True
+
+    main(clear=clear)
