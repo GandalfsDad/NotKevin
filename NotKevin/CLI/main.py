8e4e2fd01c233c181ca632ae4ce67bba446b12d1
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-05-04 06:14:39 +1000

    Add no save flag

--- NotKevin/CLI/main.py
+++ NotKevin/CLI/main.py
@@ -5,13 +5,19 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--clear", dest = "clear",help="Clears the memory before running", action="store_true")
+parser.add_argument("-n", "--nosave", dest = "nosave", help="Does not save the memory", action="store_true")
 
 def cli():
     args = parser.parse_args()
     clear = False
     if args.clear:
         clear = True
-    agent = Agent(autosave=True)
+
+    autosave = True
+    if args.NoSave:
+        autosave = False
+
+    agent = Agent(autosave=autosave)
     
     if clear:
         agent.clear_memory(save=True)
