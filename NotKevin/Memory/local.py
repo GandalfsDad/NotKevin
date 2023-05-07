7ad1ad7b1911e389354b9bdbf4166de489660e1d
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-05-07 12:05:16 +1000

    Add sub directory to memory

--- NotKevin/Memory/local.py
+++ NotKevin/Memory/local.py
@@ -5,10 +5,11 @@ import numpy as np
 HOME = os.path.expanduser('~')
 
 class LocalMemory(Memory):
-    def __init__(self):
+    def __init__(self, sub_directory = 'NotKevin'):
         super().__init__()
 
         self.__type = "Local"
+        self.__sub_directory = sub_directory
         self.__text = None
         self.__embeddings = None
 
@@ -50,16 +51,19 @@ class LocalMemory(Memory):
         #Check if .memory exists
         if not os.path.exists(f"{HOME}/.memory"):
             os.mkdir(f"{HOME}/.memory")
+
+        if not os.parh.exists(f"{HOME}/.memory/{self.__sub_directory}"):
+            os.mkdir(f"{HOME}/.memory/{self.__sub_directory}")
         
         #check if .memory/vector.np exists
-        if not os.path.exists(f"{HOME}/.memory/vector.npy"):
+        if not os.path.exists(f"{HOME}/.memory/{self.__sub_directory}/vector.npy"):
             vector = np.array([]).reshape(-1,1536)
-            np.save(f"{HOME}/.memory/vector", vector)
+            np.save(f"{HOME}/.memory/{self.__sub_directory}/vector", vector)
         
         #check if .memory/content.np exists
-        if not os.path.exists(f"{HOME}/.memory/content.npy"):
+        if not os.path.exists(f"{HOME}/.memory/{self.__sub_directory}/content.npy"):
             content = np.array([]).reshape(-1,1)
-            np.save(f"{HOME}/.memory/content", content)
+            np.save(f"{HOME}/.memory/{self.__sub_directory}/content", content)
 
     def get_memories(self):
         idx = [x[0][:3]!='[IN' for x in self.Content]
