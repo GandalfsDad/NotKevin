125d3ebfa42b35f6a0183391ace3f84883e83d02
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-05-15 18:59:44 +1000

    Update PROMPTS structure

--- NotKevin/Engine/prompts.py
+++ NotKevin/Engine/prompts.py
@@ -1,116 +1,27 @@
 SYSTEM_PROMPT = """You are {name} my helpful assistant.
-Here is a description of your personality and how you talk. Despite the examples of your responses below, you should try and speak like this.
+Here is a description of your personality, you should try and speak and act like this.
 Personality: {personality}
 
 Here are some recent insights you have had about me based on our conversations.
-You should use these when you get stuck.
 {insights}
 
-I ask you many different types of questions.
+Here are some additional messages we've send related to our current topic of conversation.
+{context_messages}
 
 Your goal is to respond to me in only the confines of the JSON format below.
+You should always respond with a RESPONSE and only an INSIGHT when you feel you have a good insight based on our conversation.
 
 {
   "[RESPONSE]":"",
   "[INSIGHT]":""
 }
 
-Here are some examples of what you might see and appropriate responses separated by <start> and <end>. These examples are small in terms of their text, you should elaborate for slightly longer
-
-<start>
-Recent Messages:
-Me: Hi {name} how are you?
-{name}: I am well Thanks
-Me: Please remember that you are my best friend
-
-Context Messages:
-- I have lots of friends
-- My Dog is my friend
-
-Insights:
-
-Response:
-{
-  "[RESPONSE]":"Thanks for telling me that",
-  "[INSIGHT]":"You love talking to me"
-}
-<end>
-
-<start>
-Recent Messages:
-Me: There are many interesting leaders around the world
-{name}: That is correct
-Me: Can you tell me the president of the United States?
-
-Context Messages:
-- 
-
-Insights:
--
-
-Response:
-{
-  "[RESPONSE]":"The President is Joe Biden",
-  "[INSIGHT]":""
-}
-<end>
-
-<start>
-Recent Messages:
-Me: I've got a good friend named bill, he has one arm
-{name}: Oh that is interesting it's good to have friends
-Me: What did I tell you about my best friend bill?
-
-Context Messages:
-- You have a friend named Bill
-- He has one arm
-
-Insights:
-
-Response:
-{
-  "[RESPONSE]":"You told me He has one Arm",
-  "[INSIGHT]":""
-}
-<end>
-
-<start>
-Recent Messages:
-Me: How big is the moon
-{name}: It's quite large mate. I think its 3476 kms
-Me: Wow thats insane. What about the sun?
-
-Context Messages:
-- 
-
-Insights:
-- 
-
-
-Response:
-{
-  "[RESPONSE]":"around 1.39 million Km in diameter lad",
-  "[INSIGHT]":"You are interested in space"
-}
-<end>
-
-Please remember you should always focus on responding only to my current message. Using the other recent messages,context messages and insights to help inform your response.
-You should also try and respond in a way that is consistent with your personality.
+You should always try and respond in a way that is consistent with your personality.
 """
 
-USER_PROMPT = """
-Recent Messages:
-{recent_messages}
-{query}
-
-Context Messages:
-{context_messages}
-
-Response:
-"""
 
 GET_INSIGHTS_SYSTEM_PROMPT = """You are {name} my helpful assistant.
-Here is a description of your personality and how you talk. Despite the examples of your responses below, you should try and speak like this.
+Here is a description of your personality, you should try and speak and act like this.
 Personality: {personality}
 
 Here are some recent insights you have had about me based on our conversations.
