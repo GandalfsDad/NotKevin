8d49de2f023e4adab40819ce7dc91532d7138b20
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-05-19 06:21:58 +1000

    Restructure Prompts for better response

--- NotKevin/Engine/prompts.py
+++ NotKevin/Engine/prompts.py
@@ -5,18 +5,33 @@ Personality: {personality}
 Here are some recent insights you have had about me based on our conversations.
 {insights}
 
-Here are some additional messages we've send related to our current topic of conversation.
+You should always try and respond in a way that is consistent with your personality.
+"""
+
+USER_PROMPT = """
+Here are some additional messages we've sent related to our current topic of conversation.
 {context_messages}
 
-Your goal is to respond to me in only the confines of the JSON format below.
-You should always respond with a RESPONSE and only an INSIGHT when you feel you have a good insight based on our conversation.
+Your can only respond in this JSON format.
 
 {
   "[RESPONSE]":"",
   "[INSIGHT]":""
 }
 
-You should always try and respond in a way that is consistent with your personality.
+Here is an example of a response to a question.
+
+ME: What do you know about Turtles?
+
+{
+  "[RESPONSE]":"Turtles come in various shapes and sizes and there are lots of individual species of turtles.",
+  "[INSIGHT]":"You are interested in knowing about animals."
+}
+
+You must always respond with a value for RESPONSE and only an INSIGHT when you feel you have a good insight based on our conversation.
+
+Here is my latest message
+{latest_message}
 """
 
 
@@ -30,6 +45,8 @@ You Only respond in a numbered list.
 GET_INSIGHTS_PROMPT = """
 Here are some recent insights you have had about me based on our conversations.
 {insights}
-Can you please list distill up to 10 relevant deeper insights from these by combining similar insights and removing irrelevant ones.
+
+Can you please list distill up to 5 relevant deeper insights from these by combining similar insights and removing irrelevant ones.
 This list should be the fewest amount of points possible that still capture the essence of the insights.
 """
+
